c4c7a6de51707f968cca5ef9ba136ddcda14d8bc
Hanna <foureaf@gmail.com>
2023-06-28 13:21:58 +0900

    Feat : user Schema에refreshToken값 저장

--- src/utils/jwtTokenMaker.ts
+++ src/utils/jwtTokenMaker.ts
@@ -8,7 +8,9 @@ type Token = {
 
 function makeJwtToken(user: any) {
   const secretKey = process.env.JWT_SECRET_KEY || 'secret-key';
-  const token = jwt.sign({ user_id: user._id, role: user.role }, secretKey);
+  const token = jwt.sign({ user_id: user._id, role: user.role }, secretKey, {
+    expiresIn: '2h',
+  });
   const userInfoWithUserToken = <Token>{};
   userInfoWithUserToken.token = token;
   userInfoWithUserToken.uuid = user.uuid;
@@ -16,3 +18,14 @@ function makeJwtToken(user: any) {
 }
 
 export { makeJwtToken };
+
+function makeRefreshJwtToken(user: any) {
+  const secretKey = process.env.JWT_SECRET_KEY || 'secret-key';
+  const token = jwt.sign({ user_id: user._id, role: user.role }, secretKey, {
+    expiresIn: '14 days',
+  });
+  const userInfoWithUserToken = <Token>{};
+  userInfoWithUserToken.token = token;
+  userInfoWithUserToken.uuid = user.uuid;
+  return userInfoWithUserToken;
+}
